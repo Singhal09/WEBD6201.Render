171b83565d87fb93f4851f1fe2645b355cd3fc51
Amritpal <Amritpal@DESKTOP-IN387AP>
2023-04-09 00:01:21 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SessionSecret = exports.HostName = exports.RemoteURI = void 0;
+exports.RemoteURI = "mongodb+srv://Sanya09:VcdVSRAR8aQGbhX9@cluster0.tabeggb.mongodb.net/contacts?retryWrites=true&w=majority";
+exports.HostName = "MongoDB Atlas";
+exports.SessionSecret = "Webd6201SessionSecret";
+//# sourceMappingURL=db.js.map
---
+++ server/config/db.ts
@@ -0,0 +1,6 @@
+// export const LocalURI ="mongodb://127.0.0.1:27017/contacts";
+export const RemoteURI ="mongodb+srv://Sanya09:VcdVSRAR8aQGbhX9@cluster0.tabeggb.mongodb.net/contacts?retryWrites=true&w=majority";
+// export const HostName="localhost";
+
+export const HostName = "MongoDB Atlas";
+export const SessionSecret = "Webd6201SessionSecret";
